ad9891b37c55ed26db2f7683991adbdb10b9419a
tauraamui <aplewis95@gmail.com>
2017-04-05 08:13:35 +0100

    working on movement

--- TacusciSandboxCore/src/main/kotlin/co/uk/tauraamui/TacusciSandbox/game/entities/Entity.kt
+++ TacusciSandboxCore/src/main/kotlin/co/uk/tauraamui/TacusciSandbox/game/entities/Entity.kt
@@ -1,5 +1,7 @@
 package co.uk.tauraamui.TacusciSandbox.game.entities
 
+import com.shc.silenceengine.core.SilenceEngine
+import com.shc.silenceengine.input.Keyboard
 import com.shc.silenceengine.math.Vector3
 
 /**
@@ -7,7 +9,16 @@ import com.shc.silenceengine.math.Vector3
  */
 open class Entity {
 
-    val position: Vector3 = Vector3()
+    val position = Vector3()
+    var maxSpeed = 1.0F
+    var acceleration = 0.2F
+    var velocity = Vector3()
 
-    fun update(delta: Float) { println(position) }
+    fun update(delta: Float) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+            velocity.x += acceleration
+            if (velocity.x > maxSpeed) { velocity.x = maxSpeed }
+        }
+        position.
+    }
 }
