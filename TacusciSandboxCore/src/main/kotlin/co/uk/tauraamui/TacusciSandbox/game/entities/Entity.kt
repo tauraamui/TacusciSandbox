acd1eddcc2e81ddc94762aa44a73dbd762f687b0
tauraamui <aplewis95@gmail.com>
2017-04-05 14:08:37 +0100

    working on entity movement

--- TacusciSandboxCore/src/main/kotlin/co/uk/tauraamui/TacusciSandbox/game/entities/Entity.kt
+++ TacusciSandboxCore/src/main/kotlin/co/uk/tauraamui/TacusciSandbox/game/entities/Entity.kt
@@ -1,6 +1,5 @@
 package co.uk.tauraamui.TacusciSandbox.game.entities
 
-import com.shc.silenceengine.core.SilenceEngine
 import com.shc.silenceengine.input.Keyboard
 import com.shc.silenceengine.math.Vector3
 
@@ -9,16 +8,32 @@ import com.shc.silenceengine.math.Vector3
  */
 open class Entity {
 
-    val position = Vector3()
-    var maxSpeed = 1.0F
-    var acceleration = 0.2F
-    var velocity = Vector3()
+    val position = Vector3(0f, 0f, 0f)
+    val direction = Vector3(0f, 0f, 0f)
+
+    val velocity = 1
+
+    var heightAngle = 0.toDouble()
+    var planeAngle = 0.toDouble()
 
     fun update(delta: Float) {
-        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            velocity.x += acceleration
-            if (velocity.x > maxSpeed) { velocity.x = maxSpeed }
-        }
-        position.
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) planeAngle -= 1
+        if (Keyboard.isKeyDown(Keyboard.KEY_D)) planeAngle += 1
+
+        direction.x = (Math.cos(planeAngle)*Math.cos(heightAngle)).toFloat()
+        direction.z = (Math.sin(planeAngle)*Math.cos(heightAngle)).toFloat()
+        direction.y = Math.sin(heightAngle).toFloat()
+
+        direction.normalize()
+
+        direction.x *= velocity
+        direction.y *= velocity
+        direction.z *= velocity
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) position.add(direction)
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) position.subtract(direction)
+
+        println(position)
     }
 }
